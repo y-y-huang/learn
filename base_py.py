272d299ddd9ddffd327f24b5fd32959f28322e3c
y-y-huang <huang_yiyuan@qq.com>
2019-08-11 17:08:10 +0800

    py4

---
+++ base_py.py
@@ -0,0 +1,11 @@
+# def area(radius):
+#     pi =3.14
+#     return pi*radius*radius
+# print(area(3))
+
+# with open("D:\learn\python学习随记四.docx", 'r',encoding='gbk',errors='ignore') as f:
+#     print(f.read())
+
+import os
+print(os.getcwd())
+
